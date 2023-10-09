fae9af26c0834e5d45c77b49ad97713bfb6f7a26
pudyasta <pudyastasatria@gmail.com>
2023-10-09 20:45:50 +0700

    fix(multi page build)

---
+++ src/render/vr.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./output.css";
+import Herbaverse from "../modules/Herbaverse";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Herbaverse />
+  </React.StrictMode>
+);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        vr: resolve(__dirname, "vr/index.html"),
+      },
+    },
+  },
+});
